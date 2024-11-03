da49c8c7336025ca100f6af3023eab4185687011
dejinouyang <dejinouyang@gmail.com>
2024-11-03 13:10:28 +0000

    Update level2.py

--- level2.py
+++ level2.py
@@ -21,19 +21,20 @@
 # Importing some useful libraries
 # Test your code: python -m Tests.test_level2
 
-import math
-import os
-import random
-import re
-import sys
 
 
 def solution(n):
-    return False
+    if n % 2 == 1: 
+        return "Weird"
+    elif 2 <= n <= 5:  
+        return "Not Weird"
+    elif 6 <= n <= 20:  
+        return "Weird"
+    elif n > 20:  
+        return "Not Weird"
 
 
 if __name__ == '__main__':
-    n = int(
-        input().strip())  # This line will read an integer inserted by the user and store it into a variable called n, which is then passed to your solution function as an argument
+    n = int(input().strip())  # This line will read an integer inserted by the user and store it into a variable called n, which is then passed to your solution function as an argument
 
     print(solution(n))  # calls the solution function with the value you insert
